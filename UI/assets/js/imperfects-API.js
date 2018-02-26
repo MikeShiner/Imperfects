5aed4a42239ecaaf250c7c67ba145ab2efb9928b
Mike Shiner <mike.shiner@hotmail.com>
2018-02-26 15:40:19 +0000

    UI sidebar fixes

--- UI/assets/js/imperfects-API.js
+++ UI/assets/js/imperfects-API.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var loadOptions = function () {
         $.get("/api/options", function (data) {
 
-            var sizeSelector = $('#size-selector');
+            var sizeSelector = $('.size-selector');
             var brandSelector = null;
 
             data.sizes.forEach(function (element) {
